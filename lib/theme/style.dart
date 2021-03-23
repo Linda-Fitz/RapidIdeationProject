8266a9892ff6431548416b39fabc33496f1abb15
lindafitz <lindafitzgerald94@gmail.com>
2021-03-23 17:56:57 +0100

    added font family to the theme

--- lib/theme/style.dart
+++ lib/theme/style.dart
@@ -15,14 +15,22 @@ ThemeData appTheme() {
     fontFamily: 'Karla',
     // text styling for headlines, titles, bodies of text, and more.
     textTheme: TextTheme(
-      headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
-      headline2: TextStyle(fontSize: 48.0, fontWeight: FontWeight.bold),
-      headline3: TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold),
-      headline4: TextStyle(fontSize: 28.0, fontWeight: FontWeight.bold),
-      headline5: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
-      headline6: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-      bodyText1: TextStyle(fontSize: 16.0, fontWeight: FontWeight.normal),
-      bodyText2: TextStyle(fontSize: 14.0, fontWeight: FontWeight.normal),
+      headline1: TextStyle(
+          fontSize: 72.0, fontFamily: 'Jellee', fontWeight: FontWeight.bold),
+      headline2: TextStyle(
+          fontSize: 48.0, fontFamily: 'Jellee', fontWeight: FontWeight.bold),
+      headline3: TextStyle(
+          fontSize: 40.0, fontFamily: 'Jellee', fontWeight: FontWeight.bold),
+      headline4: TextStyle(
+          fontSize: 28.0, fontFamily: 'Jellee', fontWeight: FontWeight.bold),
+      headline5: TextStyle(
+          fontSize: 24.0, fontFamily: 'Jellee', fontWeight: FontWeight.bold),
+      headline6: TextStyle(
+          fontSize: 20.0, fontFamily: 'Jellee', fontWeight: FontWeight.bold),
+      bodyText1: TextStyle(
+          fontSize: 16.0, fontFamily: 'Karla', fontWeight: FontWeight.normal),
+      bodyText2: TextStyle(
+          fontSize: 14.0, fontFamily: 'Karla', fontWeight: FontWeight.normal),
     ),
   );
 }
